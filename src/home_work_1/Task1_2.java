417073ded0eb28cda967a2c3020f48af8c969fbc
Yauheni Siomin <Siomin.E.V@yandex.by>
2024-06-26 15:36:16 +0300

    rename class and add Task1_2

---
+++ src/home_work_1/Task1_2.java
@@ -0,0 +1,16 @@
+package home_work_1;
+
+public class Task1_2 {
+
+    public static void main(String[] args) {
+
+        double a = 42.5;
+        double b = 10.0;
+
+//        double c = a | b;
+//
+//        System.out.println(с);
+    }
+}
+
+// ѕри компил€ции ошибка, т.к. побитовые операторы примен€ютс€ только дл€ целочисленных типов.
