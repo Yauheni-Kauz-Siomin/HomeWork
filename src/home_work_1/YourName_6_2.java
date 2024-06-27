cc283712efb48f0a44c36e7b88f5febcb140f117
Yauheni Siomin <Siomin.E.V@yandex.by>
2024-06-27 16:21:19 +0300

    add code UTF-8

--- src/home_work_1/YourName_6_2.java
+++ src/home_work_1/YourName_6_2.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class YourName_6_2 {
     public static void main(String[] args) {
-        String name1 = "Вася";
-        String name2 = "Анастасия";
+        String name1 = "пїЅпїЅпїЅпїЅ";
+        String name2 = "пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ";
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите ваше имя:");
+        System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅ:");
         String name = scanner.nextLine();
 
         if (name.equals(name1)) {
-            System.out.println("Привет!\n" + "Я так долго тебя ждал");
+            System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅ!\n" + "пїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ");
         } else if (name.equals(name2)) {
-            System.out.println("Я тебя так долго ждал");
+            System.out.println("пїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ");
         } else {
-            System.out.println("Добрый день, а вы кто?");
+            System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ, пїЅ пїЅпїЅ пїЅпїЅпїЅ?");
         }
 
     }
