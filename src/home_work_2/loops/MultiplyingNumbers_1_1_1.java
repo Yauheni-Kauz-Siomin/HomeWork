2f046c13dd4789972789aafcf3e7cd1536acefdb
Yauheni Siomin <Siomin.E.V@yandex.by>
2024-07-05 12:49:21 +0300

    внес изменения в решение задачи MultiplyingNumbers_1_1_1

--- src/home_work_2/loops/MultiplyingNumbers_1_1_1.java
+++ src/home_work_2/loops/MultiplyingNumbers_1_1_1.java
@@ -4,37 +4,30 @@ import java.util.Scanner;
 
 public class MultiplyingNumbers_1_1_1 {
     public static void main(String[] args) {
-        dataInput();
-        multiplaing();
+        checkAndFactorial();
     }
 
-
-// метод проверки введнного числа
-    public static long dataInput() {
+    public static void checkAndFactorial() {
         Scanner scn = new Scanner(System.in);
         System.out.println("Введите целое положительное число: ");
         if (scn.hasNextLong()) {
-            long n = scn.nextLong();
-            if (n < 0) {
-                System.out.println("Введите положительное целое число!");
+            long b = scn.nextLong();
+            if (b < 0) {
+                System.out.println("Введено отрицательное число, повторите ввод!");
             } else {
                 System.out.print("Результат вычисления равен: ");
+                long multi = 1;
+                while (b > 0) {
+                    multi = multi * b;
+                    b--;
+                }
+                System.out.println(multi);
             }
+
+        } else if (scn.hasNextDouble()) {
+            System.out.println("Введено не целое число, повторите ввод!"); // работает, если вводить дробное число через запятую
         } else {
-            System.out.println("Введиете целочисленное число!");
+            System.out.println("Введено НЕ число, повторите ввод!");
         }
-        return 0;
-    }
-
-
-/// метод для перемножения
-    public static void multiplaing() {
-        long multi = 1;
-        long a = 5;
-        while (a > 0) {
-            multi = multi * a;
-            a--;
-        }
-        System.out.println(multi);
     }
 }
