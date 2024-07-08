373c3e8cb94db656e382e66bab61b2010c14cf69
Yauheni Siomin <Siomin.E.V@yandex.by>
2024-07-08 12:52:48 +0300

    добавил решение задачи таблица умножения 1_6

---
+++ src/home_work_2/loops/MultiplicationTable_1_6.java
@@ -0,0 +1,45 @@
+package home_work_2.loops;
+
+public class MultiplicationTable_1_6 {
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 1;
+        int c = 0;
+        int temp = 2;
+
+        int a1 = 6;
+        int temp1 = 6;
+        int c1 = 0;
+
+
+        while (c < 10) {
+            for (int i = 0; i < 4; i++) {
+                System.out.print(a + " х " + b + " = " + (a * b) + " || ");
+                a++;
+                if (i == 3) {
+                    System.out.println();
+                    a = temp;
+                    b++;
+                    c++;
+                }
+            }
+        }
+        System.out.println("===========================================================");
+
+        while (c1 < 10) {
+            for (int i = 0; i < 4; i++) {
+                System.out.print(a1 + " х " + b + " = " + (a * b) + " || ");
+                a1++;
+                if (i == 3) {
+                    System.out.println();
+                    a1 = temp1;
+                    b++;
+                    c1++;
+                }
+            }
+        }
+    }
+}
+
+// как выровнять таблицу умножения, как предложено на картинке пока не свообразил
