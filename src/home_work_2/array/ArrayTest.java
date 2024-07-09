82a5818b24a151685d6e47058ede264f2a6a3955
Yauheni Siomin <Siomin.E.V@yandex.by>
2024-07-09 14:09:55 +0300

    добавлен ArrayTest.java для проверки выполнения задач 2.1.1.1 и 2.1.2.1.

--- src/home_work_2/array/ArrayTest.java
+++ src/home_work_2/array/ArrayTest.java
@@ -1,2 +1,30 @@
-package home_work_2.array;public class ArrayTest {
+package home_work_2.array;
+
+import java.util.Arrays;
+
+import static home_work_2.utils.ArraysUtils.arrayFromConsole;
+import static home_work_2.utils.ArraysUtils.arrayRandom;
+
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        arrayFromConsole_2_1_1_1();
+        arrayRandom_2_1_2_1();
+
+    }
+
+    public static void arrayFromConsole_2_1_1_1() {
+
+        int[] container = arrayFromConsole();
+        System.out.println(Arrays.toString(container));
+
+    }
+
+    public static void arrayRandom_2_1_2_1() {
+
+        int[] container = arrayRandom(10,100);
+        System.out.println(Arrays.toString(container));
+
+    }
+
 }
