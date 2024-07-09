fc4e16a545de73c097cc66b543db86a56ff09974
Yauheni Siomin <Siomin.E.V@yandex.by>
2024-07-09 14:15:22 +0300

    добавлены изменения в методы arrayFromConsole() и  arrayRandom(int size, int maxValueExclusion)

--- src/home_work_2/utils/ArraysUtils.java
+++ src/home_work_2/utils/ArraysUtils.java
@@ -1,13 +1,20 @@
 package home_work_2.utils;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
  * 2.1. Создать класс ArraysUtils. В этом классе не должно быть main метода. В этом классе написать следующие методы:
  * 2.1.1. Написать метод public static int[] arrayFromConsole(). Данный метод размер массива и каждый его элемент
  * запрашивает у пользователя через консоль.
- */
+ *  * 2.1.2. Написать метод public static int[] arrayRandom(int size, int maxValueExclusion). Данный метод принимает два
+ *  * аргумента. Первый (size) указывает размер массива который мы хотим получить. Второй (maxValueExclusion) указывает
+ *  * до какого числа генерировать рандомные числа.
+ *  * 2.1.2.1. Пример в отдельном классе с main. int[] container = arrayRandom(5, 100). Результат:
+ *  * В методе arrayRandom будет создан массив размером 5 с числами от 0 до 99 (использовать класс Random) и
+ *  * сохранён в переменную container.
+ *  */
 
 
 public class ArraysUtils {
@@ -25,8 +32,20 @@ public class ArraysUtils {
         }
         return container1;
     }
-}
 
+    public static int[] arrayRandom(int size, int maxValueExclusion) {
+        int[] container = new int[size];
+        Random rnd = new Random();
+        for (int i = 0; i < container.length; i++) {
+            container[i] = rnd.nextInt(maxValueExclusion);
+        }
+        for (int i = 0; i < container.length; i++) {
+        }
+        return container;
+    }
+
+
+}
 
 
 
