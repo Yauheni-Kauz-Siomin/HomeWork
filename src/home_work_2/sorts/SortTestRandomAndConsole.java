3347d659ccb79790497de1b5b3db8eafc919e035
Yauheni Siomin <Siomin.E.V@yandex.by>
2024-07-09 14:17:08 +0300

    добавлены решения задач 4.2.2 и 4.2.3

--- src/home_work_2/sorts/SortTestRandomAndConsole.java
+++ src/home_work_2/sorts/SortTestRandomAndConsole.java
@@ -1,21 +1,37 @@
 package home_work_2.sorts;
 
-import static home_work_2.array.ArrayRandom_2_1_2_1.arrayRandom;
+import java.util.Arrays;
+
+import static home_work_2.utils.ArraysUtils.arrayRandom;
 import static home_work_2.utils.ArraysUtils.arrayFromConsole;
-import static home_work_2.utils.SortsUtils.sort;
 import static home_work_2.utils.SortsUtils.shake;
+import static home_work_2.utils.SortsUtils.sort;
 
 
-public class SortTestRandomConsole {
+public class SortTestRandomAndConsole {
     public static void main(String[] args) {
         arraySortRandom();
-
+        System.out.println();
+        arraySortConsole();
 
     }
 
     public static void arraySortRandom() {
+        System.out.print("Какой массив был до сортировки: ");
         int[] container = arrayRandom(50, 100);
+        System.out.print(Arrays.toString(container));
+        System.out.println();
+        sort(container);
+        shake(container);
     }
 
+
+    public static void arraySortConsole() {
+        int[] container = arrayFromConsole();
+        System.out.print("Какой массив был до сортировки: " + Arrays.toString(container));
+        System.out.println();
+        sort(container);
+        shake(container);
+    }
 
 }
