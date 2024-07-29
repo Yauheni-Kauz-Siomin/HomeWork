1b18ab0cc87ea1d64a625f4b844324477b66cefc
Yauheni Siomin <Siomin.E.V@yandex.by>
2024-07-30 01:12:30 +0300

    Добавил решение задачи 11

--- src/home_work_3/calcs/additional/CalculatorWithCounterAutoAgregationInterface.java
+++ src/home_work_3/calcs/additional/CalculatorWithCounterAutoAgregationInterface.java
@@ -1,2 +1,57 @@
-package home_work_3.calcs.additional;public class CalculatorWithCounterAutoAgregationInterface {
+package home_work_3.calcs.additional;
+
+import home_work_3.calcs.api.ICalculator;
+
+public class CalculatorWithCounterAutoAgregationInterface {
+
+    ICalculator calculator;
+
+
+    private long countOperation = 0;
+
+
+    public CalculatorWithCounterAutoAgregationInterface(ICalculator calculator) {
+        this.calculator = calculator;
+    }
+
+    public long getCountOperation() {
+        return countOperation;
+    }
+
+    public double division(double a, double b) {
+        countOperation++;
+        return calculator.division(a, b);
+    }
+
+    public double multiplication(double a, double b) {
+        countOperation++;
+        return calculator.multiplication(a, b);
+    }
+
+    public double subtraction(double a, double b) {
+        countOperation++;
+        return calculator.subtraction(a, b);
+    }
+
+    public double addition(double a, double b) {
+        countOperation++;
+        return calculator.addition(a, b);
+    }
+
+    public double exponentiation(double a, int b) {
+        countOperation++;
+        return calculator.exponentiation(a, b);
+    }
+
+    public double absoluteValueOfNumber(double a) {
+        countOperation++;
+        return calculator.absoluteValueOfNumber(a);
+    }
+
+    public double sqrtMethod(double a) {
+        countOperation++;
+        return calculator.sqrtMethod(a);
+    }
+
 }
+
--- src/home_work_3/runners/CalculatorWithCounterAutoCompositeInterfaceMain.java
+++ src/home_work_3/runners/CalculatorWithCounterAutoCompositeInterfaceMain.java
@@ -1,2 +1,19 @@
-package home_work_3.runners;public class CalculatorWithCounterAutoCompositeInterfaceMain {
+package home_work_3.runners;
+
+import home_work_3.calcs.additional.CalculatorWithCounterAutoAgregationInterface;
+import home_work_3.calcs.api.ICalculator;
+import home_work_3.calcs.simple.CalculatorWithMathCopy;
+
+public class CalculatorWithCounterAutoCompositeInterfaceMain {
+    public static void main(String[] args) {
+
+        ICalculator calculator = new CalculatorWithMathCopy();
+        CalculatorWithCounterAutoAgregationInterface calculator10 = new CalculatorWithCounterAutoAgregationInterface(calculator);
+
+        double a = calculator10.exponentiation((calculator10.division(28, 5)), 2);
+        double b = calculator10.addition(calculator10.multiplication(15, 7), a);
+        System.out.println(calculator10.addition(4.1, b));
+        System.out.println(calculator10.getCountOperation());
+    }
+
 }
