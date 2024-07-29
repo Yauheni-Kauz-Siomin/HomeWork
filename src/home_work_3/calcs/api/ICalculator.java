f99cce8840dd35af94d0e27909f50835f9e95ef1
Yauheni Siomin <Siomin.E.V@yandex.by>
2024-07-30 01:10:39 +0300

    Добавил решение задачи 10

---
+++ src/home_work_3/calcs/api/ICalculator.java
@@ -0,0 +1,19 @@
+package home_work_3.calcs.api;
+
+public interface ICalculator {
+
+    double division(double a, double b);
+
+    double multiplication(double a, double b);
+
+    double subtraction(double a, double b);
+
+    double addition(double a, double b);
+
+    double exponentiation(double a, int b);
+
+    double absoluteValueOfNumber(double a);
+
+    double sqrtMethod(double a);
+
+}
