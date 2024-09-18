18ae7c33c22f551077465a4d076a2e94a8ecd3a1
Yauheni Siomin <Siomin.E.V@yandex.by>
2024-09-18 19:16:00 +0300

    внес изменения в реализацию пункта 2.1

--- src/home_work_6/Main.java
+++ src/home_work_6/Main.java
@@ -3,20 +3,29 @@ package home_work_6;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
 
-        String book = new String(Files.readAllBytes(new File("Война и мир_книга.txt").toPath()));
+/*        // 2.1 Найти в тексте все использованные слова и поместить их в коллекцию Set
+        Т.к. класс HashSet реализует интерфейс Set, он может хранить только уникальные значения,
+        таким образом можно говорить о количестве уникальных слов в книге*/
+
+        String filePath = "Война и мир_книга.txt";
+
+        String book = new String(Files.readAllBytes(new File(filePath).toPath()));
 
         Set<String> allWords = new HashSet<>(Arrays.asList(book.split("[\\p{Punct}\\s]+")));
 
-        System.out.println("Количество слов: " +allWords.size());
+        List<String> words = new ArrayList<>(Arrays.asList(book.split("[\\p{Punct}\\s]+"))); // для сравнения
 
+        System.out.println("Количество слов (уникальных): " +allWords.size());
+        System.out.println("Количество всего слов в книге: " +words.size());
 
     }
+
+
+
 }
